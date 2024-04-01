b1c335ead6c0358bbf3e1ea55f3a71b5b7ecec2f
YASASWINI <gyasu1110@gmail.com>
2024-04-01 16:40:48 +0530

    Update LibraryFrame.java

--- LibraryFrame.java
+++ LibraryFrame.java
@@ -126,29 +126,4 @@ public class LibraryFrame implements ActionListener{
 	}
 		}
 
-//The owner of a library wants to digitalize the process of library management, so help 
-//him in this process by witing a java program that stores the details(book id, book name, 
-//author, price) of books in a file, It should be able to add new books to file, Lend books to 
-//Readers and their details in order to lend books. The reader can search books by book 
-//name, book id. He wants to do this in proper and easy manner as he feels that compiler 
-//and terminal inputs are difficult and not easy to understand so, you have to use 
-//Graphical User Interface  
-//----------------------------------------------------------
-//|                 Library Management System              |
-//|--------------------------------------------------------|
-//| ID:   [             ]                                  |
-//| Name: [             ]                                  |
-//| Author:[             ]                                 |
-//| Price: [            ]                                  |
-//| Add Book Button                                        |
-//|--------------------------------------------------------|
-//|                      Display                                  |
-//|                                                        |
-//|--------------------------------------------------------|
-//| Search by ID: [                ]                        |
-//| Search by ID Button                                     |
-//|                                                        |
-//| Search by Name: [             ]                         |
-//| Search by Name Button                                  |
-//----------------------------------------------------------
 
